a0eb883a8380df8897f2292e0bf86532d830ffb3
Santiago Andres Coronado <santiicoronado.70@gmail.com>
2023-05-04 11:11:43 -0300

    primer commit de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstudiosComponent } from './components/estudios/estudios.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstudiosComponent,
+    FooterComponent,
+    HeaderComponent,
+    LoginComponent,
+    ProyectosComponent,
+    SkillsComponent,
+    SobreMiComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
